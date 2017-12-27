ebac692ffe628a3026ada8be07dd420c029b10fe
mcmx <sergio@sitcocr.com>
2017-12-26 18:56:10 -0600

    Added plan and subscriptions support

--- gpayments/api_resources/__init__.py
+++ gpayments/api_resources/__init__.py
@@ -8,4 +8,7 @@ from gpayments.api_resources.account import Account
 from gpayments.api_resources.customer import Customer
 from gpayments.api_resources.charge import Charge
 from gpayments.api_resources.simplecharge import SimpleCharge
+from gpayments.api_resources.plan import Plan
+from gpayments.api_resources.subscription import Subscription
+
 
